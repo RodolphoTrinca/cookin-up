3a8785ffa680f9418f1da3475cd3f3e563dc6b19
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2024-03-30 14:40:01 -0300

    Fixing tsconfig errors

--- src/components/Rodape.vue
+++ src/components/Rodape.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="rodape paragrafo">
-    Desenvolvido por Rodolpho Trinca | 2024 - Projeto fictício sem fins comerciais.
+    Desenvolvido por Alura | 2023 - Projeto fictício sem fins comerciais.
   </footer>
 </template>
 
