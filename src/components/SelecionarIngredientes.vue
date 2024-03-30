baef56ce661c7e5c9cab5d69221e6d7e0d48c461
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2024-03-30 14:28:15 -0300

    add footer and more elements

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,16 +1,20 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CardCategoria.vue';
+import BotaoPrincipal from './BotaoPrincipal.vue';
 
 export default {
   data() {
     return {
       categorias: [] as ICategoria[]
-    }
+    };
   },
   async created() {
     this.categorias = await obterCategorias();
-  }
+  },
+  components: { CardCategoria, BotaoPrincipal },
+  emits: ['adicionarIngrediente', 'removerIngrediente']
 }
 </script>
 
@@ -24,13 +28,19 @@ export default {
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        {{ categoria.nome }}
+        <CardCategoria
+          :categoria="categoria"
+          @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
+          @remover-ingrediente="$emit('removerIngrediente', $event)"
+        />
       </li>
     </ul>
 
     <p class="paragrafo dica">
       *Atenção: consideramos que você tem em casa sal, pimenta e água.
     </p>
+
+    <BotaoPrincipal texto="Buscar receitas!" />
   </section>
 </template>
 
